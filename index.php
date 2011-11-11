c402da6c506b9ccb9bf9a449972ecc96e3ebcd8f
Jonathan Goldford <jonathan.goldford@jgvisual.com>
2011-11-11 14:18:24 -0600

    Did some HTML cleanup

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php include('includes/timeline-maker.php'); ?>
-
 <!doctype html>
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
@@ -9,14 +8,10 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/b/378 -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title></title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <title>Project Timeline Creator</title>
+  <meta name="description" content="The app allows you to input the project start date, each step of the project and the turnaround times.  It will then create a table with due dates for each piece of the project.">
 
   <!-- Mobile viewport optimized: j.mp/bplateviewport -->
   <meta name="viewport" content="width=device-width,initial-scale=1">
@@ -27,11 +22,8 @@
 <body>
 
   <div id="container">
-    <header>
-
-    </header>
     <div id="main" role="main">
-      <h1>Project Timeline Generator</h1>
+      <h1>Project Timeline Creator</h1>
       <?php if (isset($_POST['start_date'])) $due_dates = build_timeline(); ?>
             
       <p>The final payment will be added <?php echo DAYS_TO_FINAL_PAYMENT; ?> calendar days after the last project step.  All turnaround times (TT) are calculated in business days.</p>
@@ -43,20 +35,11 @@
               <p><input type="button" value="Add Project Step" id="add_step" /> <input type="button" value="Delete Last Project Step" id="delete_step" /></p>
               <p><input type="submit" value="Create Timeline" /></p>
       </form>
-      
-      
     </div>
-    <footer>
-
-    </footer>
   </div> <!--! end of #container -->
 
 
-  <!-- JavaScript at the bottom for fast page loading -->
-
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
   <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
   <script src="js/scripts.js"></script>
   
@@ -66,6 +49,5 @@
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
   <![endif]-->
-  
 </body>
 </html>
