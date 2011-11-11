b86d3d2708b646e2305b86926fa4215b51789a02
Jonathan Goldford <jonathan.goldford@jgvisual.com>
2011-11-11 14:40:02 -0600

    Did some more HTML cleanup

--- index.php
+++ index.php
@@ -38,16 +38,8 @@
     </div>
   </div> <!--! end of #container -->
 
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-  <script src="js/scripts.js"></script>
-  
-  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
-       chromium.org/developers/how-tos/chrome-frame-getting-started -->
-  <!--[if lt IE 7 ]>
-    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-  <![endif]-->
+  <script src="js/scripts.js"></script>  
 </body>
 </html>
